09a12cecb4ac49e16c17066c84f23e13631dabfd
Venera <osmven@gmail.com>
2022-10-16 11:30:41 +0300

    Task06/

---
+++ Task06/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 6: Напишите программу, которая 
+1. на вход принимает число
+2. выдаёт, является ли число чётным (делится ли оно на два без остатка). 4 -> да;  -3 -> нет;  7 -> нет*/
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
