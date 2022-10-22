2aa60e11cfa203c5088190c38457367d8f17140f
Venera <osmven@gmail.com>
2022-10-22 10:22:32 +0300

    Таблица кубов чисел от 1 до N

---
+++ Task23/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и 
+выдаёт таблицу кубов чисел от 1 до N.
+
+5 ->
+1 | 1
+2 | 8
+3 | 27
+4 | 64
+5 | 125*/
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void CubesOfNumbers(int x)
+{
+    int count = 1;
+    int result = 1;
+    while (x >= count)
+    {
+        result = count * count * count;
+        System.Console.WriteLine($" {count}^3 = {result}");
+        //System.Console.WriteLine($" {count} | {result}");
+        count++;
+    }
+}
+CubesOfNumbers(number);
