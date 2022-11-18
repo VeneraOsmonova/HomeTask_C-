4cc8eb8ba868158a8f1e8c10585d6f9e175f09a4
Venera <osmven@gmail.com>
2022-11-18 13:50:26 +0300

    Программа, кот.находит сумму элементов, стоящих на нечетных позициях.

---
+++ Task36/Program.cs
@@ -0,0 +1,49 @@
+﻿
+/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0*/
+
+Console.WriteLine("Введите количество элементов массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArrayRndInt(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-1, 9);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int SumDigitPositions(int[] array)
+{
+    int position = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 > 0)
+        {
+            position = position + array[i];
+        }
+    }
+    return position;
+}
+
+int[] arr = CreateArrayRndInt(num);
+PrintArray(arr);
+int sumDigitPositions = SumDigitPositions(arr);
+Console.WriteLine(sumDigitPositions);
