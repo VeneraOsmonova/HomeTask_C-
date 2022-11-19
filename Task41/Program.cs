431456066267af610fbaa30ee8998f55deb5ebd6
Venera <osmven@gmail.com>
2022-11-19 22:27:17 +0300

    Посчитать,сколько чисел больше 0 ввёл пользователь

---
+++ Task41/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+-1, -7, 567, 89, 223-> 3*/
+
+Console.WriteLine("Введите количество чисел для ввода: ");
+int amountNumbers = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите числа, количество которых => {amountNumbers}");
+
+int[] CreateArrayInt(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int AmountOfNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] arr = CreateArrayInt(amountNumbers);
+int amountOfNumbers = AmountOfNumbers(arr);
+Console.WriteLine($"Количество чисел больше 0 => {amountOfNumbers}");
