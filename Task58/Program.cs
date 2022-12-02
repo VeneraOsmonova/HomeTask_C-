36d7ef7daf8034c71359a0ca81d83c64094bf95c
Venera <osmven@gmail.com>
2022-12-02 12:26:07 +0300

    Двумерный массив MatrixProduct

---
+++ Task58/Program.cs
@@ -0,0 +1,78 @@
+﻿/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18*/
+
+Console.WriteLine("Введите количество строк в первом двумерном массиве: ");
+int row1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в первом двумерном массиве: ");
+int column1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество строк во втором двумерном массиве: ");
+int row2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("УкажитеВведите количество столбцов во втором двумерном массиве: ");
+int column2 = Convert.ToInt32(Console.ReadLine());
+
+int[,] FillMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // 2
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) //
+        {
+            matrix[i, j] = rnd.Next(min, max + 1); // 2 - 3
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} |");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+int[,] MatrixProduct(int[,] matrix1, int[,] matrix2)
+{
+    int[,] matrixProduct = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix2.GetLength(0); k++)
+            {
+                matrixProduct[i, j] += matrix1[i, k] * matrix2[k, j];
+            }
+        }
+    }
+    return matrixProduct;
+}
+
+int[,] firstMassive2D = FillMatrixRndInt(row1, column1, 1, 10);
+int[,] secondMassive2D = FillMatrixRndInt(row2, column2, 1, 10);
+PrintMatrix(firstMassive2D);
+Console.WriteLine();
+PrintMatrix(secondMassive2D);
+
+if (column1 != row2)
+{
+    Console.WriteLine("Эти матрицы нельзя перемножить");
+}
+else
+{
+    int[,] massive2DProduct = MatrixProduct(firstMassive2D, secondMassive2D);
+    Console.WriteLine();
+    PrintMatrix(massive2DProduct);
+}
